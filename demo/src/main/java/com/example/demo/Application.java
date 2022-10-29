0dd11dcc945755c98c4d9ab52390711aa916125c
RBoghian <boghian_razvan@yahoo.com>
2022-10-29 13:31:35 +0300

    Connection Status Ok

--- demo/src/main/java/com/example/demo/Application.java
+++ demo/src/main/java/com/example/demo/Application.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello(){
+		return List.of("Hello", "SpringBoot");
 	}
 
 }
